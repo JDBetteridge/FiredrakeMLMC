d2318562a43e4a035aa325b914d969629cc325ab
Nick Twyman <NickT@Nicks-MBP.home>
2020-07-07 12:27:56 +0100

    Added scalar_problem argument and 20,000 sample MC json

--- MLMC.py
+++ MLMC.py
@@ -9,68 +9,42 @@ def MC_scalar(mesh_size, iterations, mult):
     start = time.time()
 
     mesh = UnitSquareMesh(mesh_size, mesh_size)
-
+    sample = Constant(0)
     V = FunctionSpace(mesh, "Lagrange", 4)
-    u = TrialFunction(V)
-    v = TestFunction(V)
-
-    x, y = SpatialCoordinate(mesh)
-    base_f = exp(-(((x-0.5)**2)/2) - (((y-0.5)**2)/2))
-    a = (dot(grad(v), grad(u)) + v * u) * dx
-
-    bcs = DirichletBC(V, 0, (1,2,3,4))
+    uh = Function(V)
+    vp = newProblem(mesh, sample, uh)
 
     rg = RandomGenerator(MT19937(12345))
     solutions = []
 
-    f = Constant(mult/2)*base_f
-    L = f*v*dx
-    u_real = Function(V)
-    solve(a == L, u_real, bcs=bcs, solver_parameters={'ksp_type': 'cg'})
-    e_real = assemble(Constant(0.5) * dot(u_real, u_real) * dx)
-
-    results = []
     for i in range(iterations):
         print("Sample {} of {}".format(i+1, iterations))
-        rand_multiplier = Constant(mult*rg.random_sample())
-        f = rand_multiplier*base_f
+        sample.assign(Constant(mult*rg.random_sample()))
 
-        L = f * v * dx 
-
-        uh = Function(V)
-
-        solve(a == L, uh, bcs=bcs, solver_parameters={'ksp_type': 'cg'})
-
+        vp.solve()
         energy = assemble(Constant(0.5) * dot(uh, uh) * dx)
         
         solutions.append(energy)
         estimate = sum(solutions)/(i+1)
-        error = ((estimate - e_real)*100)/estimate
-        results.append(error)
-
-    
-
-    
+   
     end = time.time()
     print("Runtime: ", end - start, "s") 
-
-
 
     fig, axes = plt.subplots()
     #axes.plot([i for i in range(iterations)], results, 'r')
     axes.hist(solutions, bins = 40, color = 'blue', edgecolor = 'black')
     plt.show()
-    """
-    FILE_NAME = "10000_int.json"
+    
+    FILE_NAME = "20000_int.json"
     with open(FILE_NAME, "w") as handle:
         json.dump(estimate, handle)
     
-    FILE_NAME = "10000_list.json"
+    FILE_NAME = "20000_list.json"
     with open(FILE_NAME, "w") as handle:
         json.dump(solutions, handle)
-    """
+    
 
-    return results
+    return 0
 
 # ============================================================================ #
 
@@ -123,7 +97,7 @@ def samp():
     return 20*rg.random_sample()
 
 
-def MLMC_general_scalar(problem, sampler, starting_mesh, levels, repititions, isEval=True):
+def MLMC_general_scalar(pde_problem, scalar_problem, sampler, starting_mesh, levels, repititions, isEval=True):
     """
     arg: problem (func) - function of problem which takes 2 arguments: mesh and 
                           and a random sample, returns scalar solution
@@ -138,7 +112,7 @@ def MLMC_general_scalar(problem, sampler, starting_mesh, levels, repititions, is
     assert len(repititions) == levels, \
     ("The levels arguement is not equal to the number of entries in repititions")
 
-    solver = MLMC_Solver(problem, starting_mesh, sampler, levels)
+    solver = MLMC_Solver(pde_problem, starting_mesh, sampler, levels, scalar_problem)
 
     # Iterate through each level in hierarchy
     for i in range(levels):
@@ -191,14 +165,15 @@ def general_test():
     V = FunctionSpace(coarse_mesh, "Lagrange", 4)
 
     #estimate = MLMC_general(V, levels, repititions, samples, True)
-    estimate = MLMC_general_scalar(newProblem, samp, V, levels, repititions, True)
+    estimate = MLMC_general_scalar(newProblem, scalarProblem, samp, V, levels, repititions, True)
 
 
 
 
 class MLMC_Solver:
-    def __init__(self, problem, coarse_mesh, sampler, levels):
-        self.problem = problem
+    def __init__(self, pde_problem, coarse_mesh, sampler, levels, scalar_problem):
+        self.pde_problem = pde_problem
+        self.scalar_problem = scalar_problem
         self._coarse_V = coarse_mesh
         self.sampler = sampler
 
@@ -212,7 +187,7 @@ class MLMC_Solver:
 
     def newLevel(self, level):
 
-        self._level_list[level] = P_level(self._coarse_V, self.problem, self.sampler, level)
+        self._level_list[level] = P_level(self._coarse_V, self.pde_problem, self.scalar_problem, self.sampler, level)
 
     def addTerm(self, level):
         self._level_list[level].calculate_term()
@@ -238,16 +213,21 @@ class MLMC_Solver:
         
         with open("10000_int.json") as handle:
             e_10000 = json.load(handle)
+        
+        with open("20000_int.json") as handle:
+            e_20000 = json.load(handle)
 
         d_10 = self._result - e_10
         d_100 = self._result - e_100
         d_1000 = self._result - e_1000
         d_10000 = self._result - e_10000
+        d_20000 = self._result - e_20000
 
         print("% difference from 10 sample MC: ",(d_10*100)/self._result)
         print("% difference from 100 sample MC: ",(d_100*100)/self._result)
         print("% difference from 1000 sample MC: ",(d_1000*100)/self._result)
         print("% difference from 10000 sample MC: ",(d_10000*100)/self._result)
+        print("% difference from 20000 sample MC: ",(d_20000*100)/self._result)
 
         convergence_tests(self._result)
 
@@ -255,30 +235,39 @@ class MLMC_Solver:
 
 class P_level:
     
-    def __init__(self, coarse_V, problem, sampler, level):
-        self.problem = problem
+    def __init__(self, coarse_V, pde_problem, scalar_problem, sampler, level):
+        # Assignments
+        self.pde_problem = pde_problem
+        self.scalar_problem = scalar_problem
         self._level = level
-        self._hierarchy = MeshHierarchy(coarse_V.mesh(), level, 1)
         self.sampler = sampler
+
+        # Create Hierarchy
+        self._hierarchy = MeshHierarchy(coarse_V.mesh(), level, 1)
 
         self._sample_counter = 0 # Needed for divison in get_average()
         self._value = None # Stores result
-        self._sample = Constant(0)
+        self._sample = Constant(0) # Stores current sample
 
         family = coarse_V.ufl_element().family()
         degree = coarse_V.ufl_element().degree()
+        # Create variational problem(s)
+        self.initialise_problem(family, degree)
         
+    
+    def initialise_problem(self, family, degree):
         # Set up fine variational problem with sample which varies
-        V_f = FunctionSpace(self._hierarchy[level], family, degree)
+        V_f = FunctionSpace(self._hierarchy[self._level], family, degree)
         self._uh_f = Function(V_f)
-        self._vs_f = self.problem(self._hierarchy[level], self._sample, self._uh_f)
+        self._vs_f = self.pde_problem(self._hierarchy[self._level], self._sample, self._uh_f)
         
         # Set up coarse variational problem with sample which varies
         if self._level-1 >= 0:
-            V_c = FunctionSpace(self._hierarchy[level-1], family, degree)
+            V_c = FunctionSpace(self._hierarchy[self._level-1], family, degree)
             self._uh_c = Function(V_c)
-            self._vs_c = self.problem(self._hierarchy[level-1], self._sample, self._uh_c) 
-    
+            self._vs_c = self.pde_problem(self._hierarchy[self._level-1], self._sample, self._uh_c) 
+
+
     def get_average(self):
         self._hierarchy = None # For memory conservation clear hierarchy
         return self._value/self._sample_counter
@@ -295,18 +284,19 @@ class P_level:
         # Generate sample and call problem fuction on sample
         self._sample.assign(Constant(self.sampler()))
         self._vs_f.solve()
-        # This part needs to be abstracted away. Perhaps a second input function?
-        e_f = assemble(Constant(0.5) * dot(self._uh_f, self._uh_f) * dx)
+        
+        e_f = self.scalar_problem(self._uh_f)
 
         if self._level-1 >= 0:  
             self._vs_c.solve()
-            e_c = assemble(Constant(0.5) * dot(self._uh_c, self._uh_c) * dx)
-
+            e_c = self.scalar_problem(self._uh_c)
             self._value +=  e_f - e_c
         else:
             self._value += e_f
         self._sample_counter += 1
         return 0
+
+# ============================================================================ #
 
 def newProblem(mesh, alpha, uh):
     V = FunctionSpace(mesh, "Lagrange", 4)
@@ -324,25 +314,26 @@ def newProblem(mesh, alpha, uh):
 
     return LinearVariationalSolver(vp, solver_parameters={'ksp_type': 'cg'})
 
-
+def scalarProblem(uh):
+    return assemble(Constant(0.5) * dot(uh, uh) * dx)
 
 def convergence_tests(param = None):
     """
     Function which compares result to 10,000 sample MC 
     """
-    with open("10000_list.json") as handle:
+    with open("20000_list.json") as handle:
             results = json.load(handle)
     
     res2 = [sum(results[:i])/(i+1) for i in range(len(results))]
     fig, axes = plt.subplots()
-    axes.plot([i for i in range(10000)], res2, 'r')
+    axes.plot([i for i in range(20000)], res2, 'r')
     if param != None:
         plt.axhline(y=param, color='b')
     #axes.hist(solutions, bins = 40, color = 'blue', edgecolor = 'black')
     plt.show()
 
 if __name__ == '__main__':
-    #print(MC_scalar(10, 500, 20))
+    #print(MC_scalar(40, 20000, 20))
     general_test()
     #convergence_tests()
 
