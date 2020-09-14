e96a3941a968b5277d1a7b142a907d980685c947
Jack Betteridge <J.D.Betteridge@bath.ac.uk>
2020-09-14 17:17:17 +0100

    Added package structure, setup is untested

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(  name='MLMCeamless',
+        version=1.0,
+        author='Nick Twyman',
+        author_email='nicholas.twyman15@imperial.ac.uk',
+        description='',
+        long_description='',
+        url='',
+        packages=find_packages(),
+        classifiers=[   'Programming Language :: Python :: 3',
+                        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+                        'Operating System :: POSIX :: Linux'
+                    ]
+        )
