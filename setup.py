d55df81dce883542a96a59b662d0a9a566f84d1d
Jack Betteridge <J.D.Betteridge@bath.ac.uk>
2020-09-29 11:37:40 +0100

    Updated LICENCE and imported mlmc in init

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-setup(  name='MLMCeamless',
+setup(  name='mlmcparagen',
         version=1.0,
         author='Nick Twyman',
         author_email='nicholas.twyman15@imperial.ac.uk',
@@ -8,6 +8,7 @@ setup(  name='MLMCeamless',
         long_description='',
         url='',
         packages=find_packages(),
+        install_requires = ['numpy', 'mpi4py'],
         classifiers=[   'Programming Language :: Python :: 3',
                         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
                         'Operating System :: POSIX :: Linux'
